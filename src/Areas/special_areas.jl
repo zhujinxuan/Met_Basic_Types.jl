f4ec0c7100fa318d6c37d0dc769e858950b2c5be
zhujinxuan <zhujinxuan@gmail.com>
2014-07-28 05:40:55 +0800

    Fix a foolish mistake

--- src/Areas/special_areas.jl
+++ src/Areas/special_areas.jl
@@ -1,5 +1,4 @@
-function Give_An_ENSO_area(p :: AnArea_Regular;whether_test :: Bool = false,
-                             lon_west :: Float64 = 110, lon_east :: Float64 = 360-70.0 ,lat_south :: Float64 = -10.0, lat_north :: Float64 = 10.0)
+function Give_An_ENSO_area(p :: AnArea_Regular ; whether_test :: Bool = false, lon_west :: Float64 = 110.0, lon_east :: Float64 = 360-70.0 ,lat_south :: Float64 = -10.0, lat_north :: Float64 = 10.0)
   ENSO_area = A_selected_area(p, lon_west,lon_east, lat_south, lat_north )
   if (whether_test)
     return ENSO_area
@@ -7,7 +6,6 @@ function Give_An_ENSO_area(p :: AnArea_Regular;whether_test :: Bool = false,
     mask = ENSO_area.data_selected_on_plon_by
     return ENSO_area
   end
-
 end
 
 export Give_An_ENSO_area
