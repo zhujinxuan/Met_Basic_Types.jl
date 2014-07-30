2e106d5d89e3392af97b3b4b7025e1dc64be16f2
zhujinxuan <zhujinxuan@gmail.com>
2014-07-30 12:34:40 +0800

    change default ENSO area

--- src/Areas/special_areas.jl
+++ src/Areas/special_areas.jl
@@ -1,4 +1,4 @@
-function Give_An_ENSO_area(p :: AnArea_Regular ; whether_test :: Bool = false, lon_west :: Float64 = 110.0, lon_east :: Float64 = 360-70.0 ,lat_south :: Float64 = -10.0, lat_north :: Float64 = 10.0)
+function Give_An_ENSO_area(p :: AnArea_Regular ; whether_test :: Bool = false, lon_west :: Float64 = 120.0, lon_east :: Float64 = 360-60.0 ,lat_south :: Float64 = -20.0, lat_north :: Float64 = 20.0)
   ENSO_area = A_selected_area(p, lon_west,lon_east, lat_south, lat_north )
   if (whether_test)
     return ENSO_area
