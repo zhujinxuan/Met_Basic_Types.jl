305832250b8035c8e41dc2cc1efb00cb59c0f776
zhujinxuan <zhujinxuan@gmail.com>
2014-08-01 16:50:47 +0800

    NPI and PDO area

--- src/Areas/special_areas.jl
+++ src/Areas/special_areas.jl
@@ -1,11 +1,14 @@
-function Give_An_ENSO_area(p :: AnArea_Regular ; whether_test :: Bool = false, lon_west :: Float64 = 120.0, lon_east :: Float64 = 360-60.0 ,lat_south :: Float64 = -20.0, lat_north :: Float64 = 20.0)
+function Give_An_ENSO_area(p :: AnArea_Regular ; lon_west :: Float64 = 120.0, lon_east :: Float64 = 360-60.0 ,lat_south :: Float64 = -20.0, lat_north :: Float64 = 20.0)
   ENSO_area = A_selected_area(p, lon_west,lon_east, lat_south, lat_north )
-  if (whether_test)
-    return ENSO_area
-  else
-    mask = ENSO_area.data_selected_on_plon_by
-    return ENSO_area
-  end
+end
+
+function Give_An_NPI_area(p :: AnArea_Regular ; lon_west :: Float64 = 160.0, lon_east :: Float64 = 360-140.0 ,lat_south :: Float64 = 30.0, lat_north :: Float64 = 65.0)
+  NPI_area = A_selected_area(p, lon_west,lon_east, lat_south, lat_north )
+end
+
+function Give_An_PDO_area( p :: AnArea_Regular ; lon_west :: Float64 = 120.0, lon_east :: Float64 = 360-120.0 ,lat_south :: Float64 = 40.0, lat_north :: Float64 = 65.0)
 end
 
 export Give_An_ENSO_area
+export Give_An_NPI_area 
+export Give_An_PDO_area
