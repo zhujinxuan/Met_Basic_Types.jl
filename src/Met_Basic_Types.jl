b783e2a4d8b2f5fa6203afc46b147af00fd073ac
zhujinxuan <zhujinxuan@gmail.com>
2014-08-23 20:37:59 +0800

    Seperating Files

--- src/Met_Basic_Types.jl
+++ src/Met_Basic_Types.jl
@@ -3,6 +3,7 @@ module Met_Basic_Types
 # package code goes here
 include("Areas/Areas.jl")
 include("Model_Operations/area.jl")
+include("often_used/all.jl")
 
 
 
---
+++ src/often_used/all.jl
@@ -0,0 +1,2 @@
+
+include("often_used_dims.jl")
