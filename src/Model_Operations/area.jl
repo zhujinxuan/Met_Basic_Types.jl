8fccde8d86936a3d33bdb0ac6523ed9e40755579
zhujinxuan <zhujinxuan@gmail.com>
2015-04-03 10:32:48 +0800

    Better Line Width

--- src/Model_Operations/area.jl
+++ src/Model_Operations/area.jl
@@ -9,7 +9,8 @@ type Updating_WorkingSet
   plat :: Array{Float64,2}
 end
 
-function update_ratio(WS :: Updating_WorkingSet, wesn_range :: Array{Float64,1}; stratage :: Symbol = :default)
+function update_ratio(WS :: Updating_WorkingSet,
+                      wesn_range :: Array{Float64,1}; stratage :: Symbol = :default)
   (lon_westforward,lon_eastforward,lat_southforward,lat_northforward) = wesn_range ;
   plon = WS.plon;
   plat = WS.plat;
@@ -27,8 +28,10 @@ function update_ratio(WS :: Updating_WorkingSet, wesn_range :: Array{Float64,1};
     d_lon[d_lon .> 180 ] = d_lon[d_lon .> 180] -360
     d_lon[d_lon .< -180 ] = 360 + d_lon[d_lon .< -180]
     d_lat = lats - lats[ilat]
-    WS.ratio[ilon,:] = ratio_lat .* linear_update_ratio(ilat, ratio_lat, lat_northforward, lat_southforward ,d_lat)
-    WS.ratio[:,ilat] = ratio_lon .* linear_update_ratio(ilon, ratio_lon, lon_westforward , lon_eastforward  ,d_lon)
+    WS.ratio[ilon,:] = ratio_lat .* linear_update_ratio(ilat, 
+                                     ratio_lat, lat_northforward, lat_southforward ,d_lat)
+    WS.ratio[:,ilat] = ratio_lon .* linear_update_ratio(ilon, 
+                                     ratio_lon, lon_westforward , lon_eastforward  ,d_lon)
     #= WS.ratio = WS.ratio - 1e-5 =#
     nothing
   else
@@ -36,7 +39,8 @@ function update_ratio(WS :: Updating_WorkingSet, wesn_range :: Array{Float64,1};
   end
 end
 
-function linear_update_ratio (i :: Int64, ratio_line :: Array{Float64},  up_to :: Float64, down_to :: Float64, distance :: Array{Float64})
+function linear_update_ratio (i :: Int64, ratio_line :: Array{Float64},  
+                              up_to :: Float64, down_to :: Float64, distance :: Array{Float64})
   m_line = ones(size(ratio_line))
   index_m = falses(size(ratio_line)) 
   if( length(distance) != i)
@@ -64,7 +68,10 @@ function linear_update_ratio (i :: Int64, ratio_line :: Array{Float64},  up_to :
   return m_line
 end
 
-function write_ratio_mask(parea :: AnArea_Regular, sst :: Array{Float64,2}; stratage :: Symbol = :default, wesn_range :: Array{Float64,1} = ones(4)*10.0, whole_map_cir_lon :: Bool = true)
+function write_ratio_mask(parea :: AnArea_Regular, sst :: Array{Float64,2}; 
+                          stratage :: Symbol = :default,
+                          wesn_range :: Array{Float64,1} = ones(4)*10.0,
+                          whole_map_cir_lon :: Bool = true)
   bask = !isnan( Selected_within(parea,sst,  map_to = :whole ))
   #= bask1 = parea.plon_selected_on_by =#
   mask = int64(bask)
