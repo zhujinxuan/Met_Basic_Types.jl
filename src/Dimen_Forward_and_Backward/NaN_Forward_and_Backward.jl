2d17a38bc43ca07deac8e9844fc24a65c854085e
zhujinxuan <zhujinxuan@gmail.com>
2015-04-08 19:19:47 +0800

    Remove Debug codes

--- src/Dimen_Forward_and_Backward/NaN_Forward_and_Backward.jl
+++ src/Dimen_Forward_and_Backward/NaN_Forward_and_Backward.jl
@@ -1,4 +1,3 @@
-
 abstract masks_Forward_Backward
 type masks_NaN_2dim <: masks_Forward_Backward
 
@@ -9,26 +8,27 @@ type masks_NaN_2dim <: masks_Forward_Backward
   maskp :: Array{Bool}
 
   rD1 :: Array{Int64,1}
-  mask2 :: Array{Bool,1}
+  mask2 :: Array{Bool,2}
 end
 
+#= ForwardD2(xar, Dim1 ) = ForwardD2(xar, [Dim1...;]); =#
 function ForwardD2(xar, Dim1 :: Array{Int64})
   mask = isnan(xar)
   D1   = Dim1
-  D2   = filter(x->!in(x,D1),[1:ndims(xar)])
-  xp   = permutedims(xar,[D1,D2])
+  D2   = filter(x->!in(x,D1),[1:ndims(xar);])
+  xp   = permutedims(xar,[D1,D2;])
   maskp = isnan(xp)
 
   x2 = reshape(xp, reduce(*,size(xar,D1...)),reduce(*,size(xar,D2...)))
-  rD1 = find((sum(isnan(x2),2)).==0)
-  mask2 = isnan(X2)
+  rD1 = find(sum(isnan(x2),2).==0)
+  mask2 = isnan(x2)
   M    = masks_NaN_2dim(D1,D2,mask,maskp,rD1,mask2)
-  return (M,r1[rD1,:])
+  return (M , x2[rD1,:])
 end
 
 function BackwardD2( r2::Array{Float64,2} , M :: masks_Forward_Backward;)
   x2 = fill(NaN,size(M.mask2))
-  x2[rD1,:] = r2
+  x2[M.rD1,:] = r2
   Xp = reshape(x2,size(M.mask,[M.Dim1, M.Dim2]...))
   #= Xar = permutedims(Xp, indexin([1:ndims(Xp)],[M.Dim1,M.Dim2])) =#
   #= Xar = permutedims(Xp, indexin([1:ndims(Xp)],[M.Dim1,M.Dim2])) =#
@@ -38,18 +38,20 @@ end
 
 function LBackwardD2( r2::Array{Float64,1} , M :: masks_Forward_Backward;)
   x2 = fill(NaN,size(M.mask2,1))
-  x2[rD1] = r2
+  x2[M.rD1] = r2
   Xp = reshape(x2,size(M.mask,[M.Dim1]...))
   return Xp
 end
 
 function LBackwardD2( r2::Array{Float64,2} , M :: masks_Forward_Backward;)
   x2 = fill(NaN,size(M.mask2,1),size(r2,2))
-  x2[rD1,:] = r2
+  x2[M.rD1,:] = r2
 
   Xp = reshape(x2,[size(M.mask,[M.Dim1]...)...,size(r2,2)]...)
   return Xp
 end
+
+export ForwardD2,BackwardD2,LBackwardD2;
 
 abstract Met_DA_Functor{N}
 abstract REG_Functor <: Met_DA_Functor{2}
@@ -69,14 +71,21 @@ function Fevaluate(EE :: Met_DA_Functor{1} ,x)
 end
 
 function Fevaluate( EE :: _EOF_Functor, xarr; nsv = 3) 
-  (S,V,D) = svds(xarr,nsv=nsv)
+  #= (S,V,D) = svds(xarr,nsv=nsv) =#
+  xarr = broadcast(-,xarr, mean(xarr,2))
+  (S,V,D) = svd(xarr); 
+  S = S[:,1:nsv]; V = V[1:nsv]; D= D[:,1:nsv];
   ratio = V.^2/sum(xarr.^2)
   return(S, V, D, ratio)
 end
 
 function Fevaluate( FF :: _SVD_Functor, xarr, yarr; nsv = 3) 
+  xarr = broadcast(-,xarr, mean(xarr,2))
+  yarr = broadcast(-,yarr, mean(yarr,2))
   covxy = xarr*yarr'
-  (Sx,V,Sy) = svds(covxy,nsv=nsv)
+  #= (Sx,V,Sy) = svds(covxy,nsv=nsv) =#
+  (Sx,V,Sy) = svd(covxy); 
+  Sx = Sx[:,1:nsv]; V = V[1:nsv]; Sy = Sy[:,1:nsv];
   PCx = Sx'*xarr
   PCy = Sy'*yarr
   ratio = V.^2/sum(covxy.^2)
@@ -84,8 +93,8 @@ function Fevaluate( FF :: _SVD_Functor, xarr, yarr; nsv = 3)
 end
 
 function DFevaluate( EE :: _EOF_Functor,xarr, Dim1, ;nsv = 3)
-  (M,r1) = ForwardD2(xarr, Dim1)
-  (S,V,D,ratio) = Fevaluate(EE, r1)
+  (M,real1D) = ForwardD2(xarr, Dim1)
+  (S,V,D,ratio) = Fevaluate(EE, real1D,nsv = nsv)
   S2 = LBackwardD2(S,M)
   return (S2,V,D,ratio)
 end
@@ -93,7 +102,7 @@ end
 function DFevaluate ( SS :: _SVD_Functor, xarr, yarr, Dim1x, Dim1y; nsv = 3)
   (Mx, rx1) = ForwardD2(xarr,Dim1x)
   (My, ry1) = ForwardD2(yarr,Dim1y)
-  (Sx, Sy, V, ratio, PC1x, PC1y) = Fevaluate(SS, rx1,ry1)
+  (Sx, Sy, V, ratio, PC1x, PC1y) = Fevaluate(SS, rx1,ry1, nsv= nsv)
   S2x = LBackwardD2(Sx,Mx)
   S2y = LBackwardD2(Sy,My)
   Pctx = reshape(Pc1x,size(Mx.mask,Mx.Dim2))
