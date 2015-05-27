3c2b7e23f88fc150feb185841bfa9c78094b601f
zhujinxuan <zhujinxuan@gmail.com>
2015-05-27 15:11:12 +0800

    ENSO added

--- src/Areas/special_areas.jl
+++ src/Areas/special_areas.jl
@@ -32,3 +32,10 @@ function Give_An_Nino34_area ( p :: AnArea_Regular;
 end
 
 export Give_An_Nino3_area,Give_An_Nino4_area, Give_An_Nino34_area
+
+function Give_An_ENSO_area(p :: AnArea_Regular;
+                          lon_west :: Float64 = 120.0, lon_east :: Float64 = 300.0,
+                          lat_south :: Float64 = -20.0, lat_north :: Float64 = 20.0)
+  return  A_selected_area(p, lon_west,lon_east, lat_south, lat_north )
+end
+export Give_An_ENSO_area
